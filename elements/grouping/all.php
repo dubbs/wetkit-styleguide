68cea7e2eb048153a5502366531284aefd78c2ae
Kevin Warrington <kevin.warrington@gmail.com>
2014-08-12 15:58:40 -0600

    add all grouping and text elements

---
+++ elements/grouping/all.php
@@ -0,0 +1,18 @@
+<?php include '../../includes/head.php' ?>
+
+<?php patternize('P',          '01-p.html'); ?>
+<?php patternize('HR',         '02-hr.html'); ?>
+<?php patternize('PRE',        '03-pre.html'); ?>
+<?php patternize('BLOCKQUOTE', '04-blockquote.html'); ?>
+<?php patternize('OL',         '05-ol.html'); ?>
+<?php patternize('UL',         '06-ul.html'); ?>
+<?php //patternize('LI',         '07-li.html'); ?>
+<?php patternize('DL',         '08-dl.html'); ?>
+<?php //patternize('DT',         '09-dt.html'); ?>
+<?php //patternize('DD',         '10-dd.html'); ?>
+<?php patternize('FIGURE',     '11-figure.html'); ?>
+<?php //patternize('FIGCAPTION', '12-figcaption.html'); ?>
+<?php //patternize('MAIN',       '13-main.html'); ?>
+<?php //patternize('DIV',        '14-div.html'); ?>
+
+<?php include '../../includes/footer.php' ?>
