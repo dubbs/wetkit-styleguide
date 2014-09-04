d57fa33fff4ea1e052299e65bc7c905b98bd471f
Kevin Warrington <kevin.warrington@gmail.com>
2014-09-04 17:00:49 -0600

    add ids to patterns

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,12 +1,20 @@
 <?php
 
+function slugify($string)
+{
+	return preg_replace('/[^A-Za-z0-9-]+/', '-', $string);
+}
+
 function patternize($title, $filename) {
-	echo '<div class="pattern">' . "\n";
+	echo '<div class="pattern" id="'.slugify($title).'"">' . "\n";
 	echo ' <details class="pattern-details">' . "\n";
-	echo '  <summary class="pattern-name">'.$title.'</summary>' . "\n";
+	echo '  <summary class="pattern-name">'.$title.'<a href="#'.slugify($title).'">#</a></summary>' . "\n";
+
 	echo '  <pre><code class="language-markup">' . "\n";
+
 	echo htmlspecialchars(file_get_contents($filename));
 	echo '  </code></pre>' . "\n";
+
 	echo ' </details>' . "\n";
 	include $filename;
 	echo "\n" . '</div>' . "\n\n";
